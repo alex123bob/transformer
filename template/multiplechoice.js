97c655d586bc0f17a0ed9570c7c9ebd6ee2f27dc
Alexander.Li <alexander.li@CNSHHQ-0291.ef.com>
2015-05-25 11:51:37 +0800

    add picture resources and make adjustments on position of responses.

--- template/multiplechoice.js
+++ template/multiplechoice.js
@@ -5,11 +5,11 @@ Template.contactEntry = [
 	'<div class="response">',
 		'<div class="slickCt">',
 			'<div class="questionCt autoplay slider">',
-				'<div class="text_option"></div>',
-				'<div class="text_option"></div>',
-				'<div class="text_option"></div>',
-				'<div class="text_option"></div>',
-				'<div class="text_option"></div>',
+				'<div class="text_option">Listen.</div>',
+				'<div class="text_option">Stand up.</div>',
+				'<div class="text_option">Raise your hand.</div>',
+				'<div class="text_option">Be quiet.</div>',
+				'<div class="text_option">Sit down.</div>',
 			'</div>',
 			'<div class="questionCt autoplay slider">',
 				'<div class="text_option"></div>',
@@ -31,14 +31,20 @@ Template.contactEntry = [
 
 $(function (){
 	$('.carrier').append(Template.contactEntry);
+	$('.stimulus').css('background', 'url(./image/mediaresources/test1.png) no-repeat center center');
 	$('.response>.slickCt').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		autoplay: false,
 		// autoplaySpeed: 2000,
-		prevArrow: false,
-		nextArrow: false,
+		// prevArrow: false,
+		// nextArrow: false,
 		dots: true,
-		touchThreshold: 200
+		touchThreshold: 200,
+		swipe: false,
+		mobileFirst: true
+	});
+
+	$('.response>.slickCt').on('beforeChange', function (){
 	});
 });
